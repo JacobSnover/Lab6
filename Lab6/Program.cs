1ffc95072da7330ca416e1f4c9b748099eddded5
Jacob R. Snover <snoverjacob@yahoo.com>
2017-10-29 23:30:05 -0400

    Cleaned code up, made continue in to a method

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -10,55 +10,84 @@ namespace Lab6
     {
 
         static void Main(string[] args)
-        {            //string vowel = ("aeiou");
-            //prompt for word
-            //Translates to pig latin
-            //convert to lower before translating
-            //starts with vowel add way to end
-            //starts with consonant move all consonants before vowel to end, and add ay
-            //translate another word
-            Console.Write("enter word: ");
-            string input = Console.ReadLine();
-            while (true)
+        {
+            do
             {
-                if (!int.TryParse(input, out int nums))
+                //prompt for word
+                Console.Write("enter word: ");
+                string input = Console.ReadLine();
+                while (true)
                 {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("wrong");
-                    Console.Write("enter word: ");
-                    input = Console.ReadLine().ToLower();
-                }
-            }
-            var data = input.ToLower();
-            string result = "";
-            for (int i = 0; i < data.Length - 1; i++)
-            {
-                char letter = data[i];
-                if (isVowel(letter))
-                {
-                    if (i == 0)
+                    if (int.TryParse(input, out int nums) || input == " " || input.Contains('@'))
                     {
-                        result = input + "w";
+                        Console.WriteLine("wrong");
+                        Console.Write("enter word: ");
+                        input = Console.ReadLine();
+                    }
+                    else
+                    {
                         break;
                     }
-                    var afterVowel = input.Substring(i);
-                    var beforeVowel = input.Substring(0, i);
-                    result = afterVowel + beforeVowel;
                 }
 
+                //convert to lower before translating
+                var data = input.ToLower();
+                string result = "";
 
-            }
+                //Translates to pig latin
+                for (int i = 0; i < data.Length - 1; i++)
+                {
+                    char letter = data[i];
+                    if (isVowel(letter))
+                    {
+                        if (i == 0)
+                        {
+                            result = input + "w";
+                            break;
+                        }
+                        var afterVowel = input.Substring(i);
+                        var beforeVowel = input.Substring(0, i);
 
-            Console.WriteLine(result + "ay");
+                        result = afterVowel.Trim() + beforeVowel.Trim();
+                        break;
+                    }
+                }
+
+                //starts with vowel add way to end
+                //starts with consonant move all consonants before vowel to end, and add ay
+                Console.WriteLine(result + "ay");
+                Console.WriteLine(Continue());
+
+            } while (true);
 
         }
 
+        //check for vowel
         private static bool isVowel(char letter)
         {
             return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+        }
+
+        //translate another word
+        public static string Continue()
+        {
+            while (true)
+            {
+                Console.Write("Do you want to translate another word? Yes or No: ");
+                string test = Console.ReadLine().ToLower();
+
+                if (test == "y" || test == "yes")
+                {
+                    return "Sounds good. ";
+                }
+                else if (test == "n" || test == "no")
+                {
+                    Console.WriteLine("Bye");
+                    Console.ReadLine();
+                    Environment.Exit(1);
+                }
+
+            }
         }
     }
 }
